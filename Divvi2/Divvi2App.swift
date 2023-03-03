28adde0af3d0c0a234ebe7f4edabb30ad4978476
Brian LeGrand <brian.legrand@icloud.com>
2023-03-03 12:53:50 -0800

    Initial Commit

---
+++ Divvi2/Divvi2App.swift
@@ -0,0 +1,17 @@
+//
+//  Divvi2App.swift
+//  Divvi2
+//
+//  Created by Brian LeGrand on 3/3/23.
+//
+
+import SwiftUI
+
+@main
+struct Divvi2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
